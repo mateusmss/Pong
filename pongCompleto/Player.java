916bb8b9e2128823de80dda1683884d6286a9d41
mateusmss <mateusaldanha@usp.br>
2021-06-28 01:14:09 -0300

    Mudando moveUp e moveDown para o player não bugar mais

--- pongCompleto/Player.java
+++ pongCompleto/Player.java
@@ -1,10 +1,5 @@
 import java.awt.*;
 
-/**
-	Esta classe representa os jogadores (players) do jogo. A classe princial do jogo (Pong)
-	instancia dois objetos deste tipo quando a execução é iniciada.
-*/
-
 public class Player {
 	private double cx;
 	private double cy;
@@ -14,19 +9,6 @@ public class Player {
 	private double speed;
 	private Color color;
 	private double [] v_limit;
-	//private long delta;
-	/**
-		Construtor da classe Player.
-
-		@param cx coordenada x da posição inicial do player (centro do retangulo que o representa).
-		@param cy coordenada y da posição inicial do player (centro do retangulo que o representa).
-		@param width largura do retangulo que representa o player.
-		@param height altura do retangulo que representa o player.
-		@param color cor do player.
-		@param id uma string que identifica o player
-		@param v_limit um array de double contendo dois valores (em pixels) que determinam os limites verticais da área útil da quadra.
-		@param speed velocidade do movimento vertical do player (em pixels por millisegundo).
-	*/
 
 	public Player(double cx, double cy, double width, double height, Color color, String id, double [] v_limit, double speed){
 		this.cx = cx;
@@ -40,98 +22,49 @@ public class Player {
 
 	}
 
-	/**
-		Método chamado sempre que o player precisa ser (re)desenhado.
-	*/
-
 	public void draw(){
 		GameLib.setColor(this.color);
 		GameLib.fillRect(this.cx,this.cy,this.width,this.height);
 	}
 
-	/**
-		Método chamado quando se deseja mover o player para cima.
-		Este método é chamado sempre que a tecla associada à ação
-		de mover o player para cima estiver pressionada.
-
-		@param delta quantidade de millisegundos que se passou entre o ciclo anterior de atualização do jogo e o atual.
-	*/
 
 	public void moveUp(long delta){
-
-		double moviment = this.speed * delta;
-		if(((getCy() - moviment) - (getHeight()/2)) < v_limit[0]) {
-				cy = v_limit[0] - (getHeight()/2);
+		double movimento = this.speed * delta;
+		if(((getCy() - (getHeight()/2)) - movimento) > v_limit[0]) {
+				this.cy = getCy() - movimento;
 			}else{
-				cy = getCy() + moviment;
+				this.cy = v_limit[0] + (getHeight()/2);
 			}
-
+			//v_limit[0] significa a parte de cime da área do jogo
 	}
-
-	/**
-		Método chamado quando se deseja mover o player para baixo.
-		Este método é chamado sempre que a tecla associada à ação
-		de mover o player para baixo estiver pressionada.
-
-		@param delta quantidade de millisegundos que se passou entre o ciclo anterior de atualização do jogo e o atual.
-	*/
 
 	public void moveDown(long delta){
-		double moviment = this.speed * delta;
-			if(((getCy() - moviment) - (getHeight()/2)) < v_limit[0]) {
-				cy = v_limit[0] - (getHeight()/2);
-			}else{
-				cy = getCy() - moviment;
-			}
+		double movimento = this.speed * delta;
+		if(((getCy() + (getHeight()/2)) + movimento) < v_limit[1]){
+			this.cy = getCy() + movimento;
+		}else{
+			this.cy = v_limit[1] - (getHeight()/2);
+		}
+		//v_limit[1] significa a parte de baixo da área do jogo
 	}
 
-	/**
-		Método que devolve a string de identificação do player.
-		@return a String de indentificação.
-	*/
-
 	public String getId() {
-
 		return this.id;
 	}
 
-	/**
-		Método que devolve a largura do retangulo que representa o player.
-		@return um double com o valor da largura.
-	*/
-
 	public double getWidth() {
-
 		return this.width;
 	}
 
-	/**
-		Método que devolve a algura do retangulo que representa o player.
-		@return um double com o valor da altura.
-	*/
-
 	public double getHeight() {
-
 		return this.height;
 	}
 
-	/**
-		Método que devolve a coordenada x do centro do retangulo que representa o player.
-		@return o valor double da coordenada x.
-	*/
-
 	public double getCx() {
-
 		return this.cx;
 	}
 
-	/**
-		Método que devolve a coordenada y do centro do retangulo que representa o player.
-		@return o valor double da coordenada y.
-	*/
-
 	public double getCy() {
-
 		return this.cy;
 	}
 }
